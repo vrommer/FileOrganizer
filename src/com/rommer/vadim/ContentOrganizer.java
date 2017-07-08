50954209ea1aeb8560cb71f1815197c485426432
Vadim Rommer <vadim.rommer@gmail.com>
2017-07-08 10:34:56 +0300

    Adding new files

---
+++ src/com/rommer/vadim/ContentOrganizer.java
@@ -0,0 +1,5 @@
+package com.rommer.vadim;
+
+public class ContentOrganizer {
+	
+}
---
+++ src/com/rommer/vadim/ContentType.java
@@ -0,0 +1,17 @@
+//package com.rommer.vadim;
+//
+//public enum ContentType {
+//	PDF		(), 
+//	EPUB	(), 
+//	ISO		(), 
+//	MKV		(), 
+//	AVI		(),
+//	MP4		();
+//	
+//	private final String type;
+//	private ContentType(String type) {
+//		this.type = type;
+//	}
+//	
+//	public String type() { return type; }
+//}
